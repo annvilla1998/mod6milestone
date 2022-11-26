8521e9f28a4bb8624db91cce7b5046fe038bd1dc
Anabel Villalobos <88516795+annvilla1998@users.noreply.github.com>
2022-11-26 12:45:01 -0800

    created player loop

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -12,3 +12,11 @@ rooms = {
 
 currentRoom = 'Great Hall'
 
+while True:
+    #print what room the player is currently in
+    print("You are in the {}".format(currentRoom))
+    print("Make your move.")
+    #take in player's move
+    player_move = input()
+    moveList = player_move.split(" ")
+
