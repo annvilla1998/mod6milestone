f5bcbf60866e49a3f12692b77acc2330f4138a9c
Anabel Villalobos <88516795+annvilla1998@users.noreply.github.com>
2022-11-26 12:58:53 -0800

    finished player loop

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -10,13 +10,31 @@ rooms = {
         'Cellar': {'West': 'Bedroom'}
     }
 
-currentRoom = 'Great Hall'
+current_room = 'Great Hall'
 
 while True:
+    #if the user input was exit the game will end
+    if current_room == 'exit':
+        break
+
     #print what room the player is currently in
     print("You are in the {}".format(currentRoom))
     print("Make your move.")
     #take in player's move
     player_move = input()
-    moveList = player_move.split(" ")
+    move_arr = player_move.split(" ")
+    if move_arr[0] == "go":
+        direction = move_arr[1]
+        #if the direction is in the current room dictionary
+        if direction in rooms.current_room:
+            #set the current room equal to that room
+            current_room = rooms.direction
+    # if the player input is exit
+    elif move_arr[0].lower() == "exit":
+        #then the current room is set to exit
+        current_room = "exit"
+    else:
+        # handle input validation error
+        print("You can't go that way")
+
 
